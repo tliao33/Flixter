3880ffcc6fd751cd580b3840f3110c4ba286fab1
Onwuosiuno Ikhioda <suinoik@fb.com>
2022-06-15 10:02:54 -0700

    Initial Commit

---
+++ Flixter/SceneDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SceneDelegate.h
+//  Flixter
+//
+//  Created by Onwuosiuno Ikhioda on 6/15/22.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
---
+++ Flixter/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  Flixter
+//
+//  Created by Onwuosiuno Ikhioda on 6/15/22.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
